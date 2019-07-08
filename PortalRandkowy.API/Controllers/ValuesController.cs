0bed7bcd5c45fc2e20d257a4acde9eacb3eed75b
jarekg1963 <jarekg@test.com.pl>
2019-07-08 13:59:38 +0200

    Popisanie metod do controlera getPostPutDelete

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -3,44 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
 
-namespace PortalRandkowy.API.Controllers
-{
+namespace PortalRandkowy.API.Controllers {
     // nasza aplikacja bedzie dostepna pod adresem hhtp://localhost:5000/api/Values ( pierwsza czesc nazwy )
-    [Route("api/[controller]")]
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
-    {
+    public class ValuesController : ControllerBase {
+        private readonly DataContext _context;
+
+        public ValuesController (DataContext context) {
+            _context = context;
+
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {            
-            return new string[] { "value1", "value2" , "value3", "values4"};
+        public IActionResult GetValues () 
+        {
+        var values = _context.Values.ToList();
+        return Ok (values);    
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
+        [HttpGet ("{id}")]
+        public IActionResult GetValue (int id) {
+            var  Values = _context.Values.FirstOrDefault(x => x.Id == id); 
+          return Ok (Values); 
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public IActionResult  AddValue ([FromBody] Value value) 
+        { 
+            _context.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
         }
+
+
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut ("{id}")]
+        public IActionResult EditValue (int id, [FromBody] Value value) 
         {
-        }
+            var data = _context.Values.Find(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            _context.SaveChanges();
+            return Ok(data);
+
+         }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete ("{id}")]
+        public IActionResult DeleteValue (int id)
         {
-        }
+            var data = _context.Values.Find(id);
+
+            if (data == null)
+            return NoContent();
+
+            
+            _context.Values.Remove(data);
+            _context.SaveChanges();
+            return Ok(data);
+
+         }
     }
 }
