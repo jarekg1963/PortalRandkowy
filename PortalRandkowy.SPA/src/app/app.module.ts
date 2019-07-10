a2d4a56c2064f4615fc6dc268548a5769f953931
jarekg1963 <jarekg@test.com.pl>
2019-07-10 16:30:35 +0200

    Utworzenie strony do logowania

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule,
+      HttpClientModule
    ],
    providers: [],
    bootstrap: [
