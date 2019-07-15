67896fa55b0923ae467cb986554ab293155188e1
jarekg1963 <jarekg@test.com.pl>
2019-07-15 12:09:51 +0200

    Komunikacja rodzic-dziecko

--- PortalRandkowy.SPA/src/app/home/home.component.ts
+++ PortalRandkowy.SPA/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,13 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode = false;
-  constructor() { }
+values: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getValues() {
+    this.http.get('http://localhost:5000/api/Values').subscribe(
+      response => {this.values = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
