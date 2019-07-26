5748f1612eae14e908ef7c5697925bd3a7040717
jarekg1963 <jarekg@test.com.pl>
2019-07-26 15:45:18 +0200

    Utworzenie kontolera Users

--- PortalRandkowy.API/Startup.cs
+++ PortalRandkowy.API/Startup.cs
@@ -26,7 +26,10 @@ namespace PortalRandkowy.API {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
             services.AddDbContext<DataContext> (x => x.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
-            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors ();
             services.AddTransient<Seed> ();
             services.AddScoped<IAuthRepository, AuthRepository> ();
