1bb010d15db0ba917a641d8243646588223592c8
Jaroslaw J. G. Granat <jaroslaw.granat@tpvaoc.com>
2019-09-03 15:13:16 +0200

    Upload zdjecia koniec lekcja 79

--- PortalRandkowy.SPA/src/app/users/photos/photos.component.ts
+++ PortalRandkowy.SPA/src/app/users/photos/photos.component.ts
@@ -1,6 +1,6 @@
+import { Photo } from './../../_models/photo';
 import { AuthService } from './../../_services/auth.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { Photo } from 'src/app/_models/photo';
 import { DomSanitizer } from '@angular/platform-browser';
 import { FileUploader } from 'ng2-file-upload';
 
@@ -21,7 +21,7 @@ import { FileUploader } from 'ng2-file-upload';
 export class PhotosComponent implements OnInit {
 
 @Input() photos: Photo[];
- uploader: FileUploader ;
+uploader: FileUploader ;
 hasBaseDropZoneOver =  false;
 baseUrl =  'http://localhost:5000/api/'
 
@@ -46,7 +46,20 @@ initializeUploader() {
     autoUpload: false,
     maxFileSize: 10 * 1024 * 1024
   });
+  this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+  this.uploader.onSuccessItem = (item, respose, status, headers) => {
+    if (respose) {
+      const res: Photo = JSON.parse(respose);
+      const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+      this.photos.push(photo);
+    }
+  };
 }
-
 
 }
